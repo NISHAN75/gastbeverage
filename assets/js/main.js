45269027d6e1f21f16b307df35f94e85d43b7307
nishan das <nishandas880@gmail.com>
2025-03-24 15:49:22 +0600

    all is ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,56 @@
     $(document).ready(function () {
 
 	
-      
 
 
- 
+     	// offcanvas humbarger
+         let offcanvasElement = $('.header-offcanvas');
+         offcanvasElement.on('show.bs.offcanvas', function () {
+             $('.humbarger-btn').addClass('open');
+             $('.btn-close span:nth-child(1)').css({
+                 transform: 'rotate(45deg)',
+                 marginBottom: '0'
+             });
+             $('.btn-close span:nth-child(2)').css({
+                 transform: 'rotate(-45deg)',
+                 marginTop: '-4px'
+             });
+         });
+         offcanvasElement.on('hide.bs.offcanvas', function () {
+             $('.humbarger-btn').removeClass('open');
+             $('.btn-close span:nth-child(1)').css({
+                 transform: '',
+                 marginBottom: ''
+             });
+             $('.btn-close span:nth-child(2)').css({
+                 transform: '',
+                 marginTop: ''
+             });
+         });
+        // offcanvas menu 
+         $(".mobile-nav > ul > li > a").click(function (e) {
+         e.preventDefault();
+         let subMenu = $(this).next(".sub-menu");
+         $(".mobile-nav .sub-menu").not(subMenu).slideUp();
+         $(".mobile-nav > ul > li > a").not(this).removeClass('rotate active');
+         $(this).toggleClass('rotate active');
+         subMenu.stop(true, true).slideToggle();
+       });
 
-
+        //   line animation
+        const $footerShape = $('.footer-bottom-shape-info');
+        const imageWidth = 100; // Set this to the actual width of your background image in pixels
+        let position = 0;
+    
+        function animateFooter() {
+            position -= 1; 
+            $footerShape.css('background-position', `${position}px 0`);
+            if (Math.abs(position) >= imageWidth) {
+                position = 0;
+            }
+            requestAnimationFrame(animateFooter);
+        }
+        animateFooter();
         var videoCompressor = new Swiper(".video-compressor-slider", {
             pagination: {
               el: ".swiper-pagination",
@@ -19,21 +63,31 @@
             },
           });
 
+          var gastAdvantage = new Swiper(".gast-advantage-slider", {
+            pagination: {
+              el: ".swiper-pagination",
+            },
+          });
+
        // nice select
        $('select').niceSelect();
 
        gsap.registerPlugin(ScrollTrigger);
        
-       gsap.to(".line-animation-wrapper .line-wrapper", {
-        width: "calc(100% - 36px)",
-        duration: 1.2,
-        ease: "power2.out",
-        scrollTrigger: {
-            trigger: ".line-animation-wrapper",
-            start: "top 60%", // Adjust as needed
-            toggleActions: "play none none reverse"
-        }
-    });
+       const lineWrappers = document.querySelectorAll(".line-animation-wrapper .line-wrapper");
+       // Loop through each element and apply the GSAP animation
+       lineWrappers.forEach((lineWrapper) => {
+           gsap.to(lineWrapper, {
+               width: "calc(100% - 36px)", 
+               duration: 1.2, 
+               ease: "power2.out",
+               scrollTrigger: {
+                   trigger: ".line-animation-wrapper",
+                   start: "top 60%", 
+                   toggleActions: "play none none reverse" 
+               }
+           });
+       });
 
         // OverlayScrollbars
         const {
@@ -77,7 +131,10 @@
         // lenis
 
 
-        
+        $(".back-btn").on("click", function (e) {
+			e.preventDefault();
+			lenis.scrollTo(0)
+		});
 
         
     });
